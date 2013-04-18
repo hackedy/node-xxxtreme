be6037407387e9ba7e531e58a28f60f3d73535ca
Ryan Doenges <rhdoenges@gmail.com>
2013-04-17 20:38:51 -0700

    handle unicode escape sequences for x and X
    FIXES #3 (I WANT TO SAY FIXXXXXXES BUT I DONT THINK GITHUB IS SUFFICIENTLY
    EXXXXXXXXXXXXXXXXXXXXXXXXXXTREME TO HANDLE THAT)

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var xtreme_fs = require('fs');
 require.extensions['.js'] = function(module, filename) {
   var xtreme_data = xtreme_fs.readFileSync(filename).toString();
-  var plain_data = xtreme_data.replace(/x+/g, 'x').replace(/X+/g, 'X');
+  var plain_data = xtreme_data.replace(/(x|\\u0078)+/g, 'x').replace(/(X|\\u0058)+/g, 'X');
   module._compile(plain_data, filename);
 };
